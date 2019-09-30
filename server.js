cadf53d749065053b2029e507c4f53ded7d1a1d8
Mahendra801 <mahendra.angular@gmail.com>
2019-09-30 15:46:32 +0530

    fgfdgf

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(express.static(__dirname + '/janata-app'));
 app.get('*', function(req,res) {
  // Replace the '/dist/<to_your_project_name>/index.html'
  res.sendFile(path.join(__dirname + '/janata-app/index.html'));
-});
+}); 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
